bcbde034bb611c8632774f9578584649624e67e4
JSCC <juliocosta1@gmail.com>
2020-07-30 22:12:50 +0100

    nut styling

--- components/Guitar/Guitar.js
+++ components/Guitar/Guitar.js
@@ -25,6 +25,15 @@ const guitarString = css`
   height: 50px;
 `;
 
+const tdStyle = css`
+  // width: 15px;
+  // height: 1px;
+  // background-color: black;
+  // position: absolute;
+  // top: 50%;
+  // line-height: 2px;
+`;
+
 // calculate row height based on number of rows
 /**
  * API
@@ -46,17 +55,32 @@ const guitarString = css`
 // TODO: apply nut
 // TODO: apply graded fret sizing
 
-const fretGenerator = (numberOfFrets) => {
+const strings = {
+  0: "E",
+  1: "B",
+  2: "G",
+  3: "D",
+  4: "A",
+  5: "E",
+};
+
+const fretGenerator = (numberOfFrets, stringNumber) => {
   let frets = [...new Array(numberOfFrets)];
 
-  return frets.map((fret) => <td>a fret</td>);
+  return frets.map((fret, fretNumber) => {
+    if (fretNumber === 0) {
+      return <td className={tdStyle}>{strings[stringNumber]}</td>;
+    } else {
+      return <td>a fret</td>;
+    }
+  });
 };
 
 const stringLaneGenerator = (numberOfStrings) => {
   let strings = [...new Array(numberOfStrings)];
 
-  return strings.map((string) => (
-    <tr className={guitarString}>{fretGenerator(22)}</tr>
+  return strings.map((string, stringNumber) => (
+    <tr className={guitarString}>{fretGenerator(22, stringNumber)}</tr>
   ));
 };
 
